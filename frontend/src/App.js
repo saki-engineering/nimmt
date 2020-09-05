477c13615d67d009b22c737dfcb6c8ba744396f7
Hsakihara <sakiharu31415@yahoo.co.jp>
2020-09-05 14:55:54 +0900

    デフォルトheaderの削除

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,7 @@
 import React, { useReducer } from 'react';
-import logo from './logo.png';
 import './App.css';
 import { AppContext } from './contexts/appContexts';
 import { AppReducer } from './reducers/AppReducer';
-import HelloWorld from './components/HelloWorld';
 import CountBoard from './components/CountBoard';
 
 function App() {
@@ -16,14 +14,6 @@ function App() {
   return (
     <div id="app" className="App">
       <AppContext.Provider value={{state, dispatch}}>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Welcome to your new <code>wails/react</code> project.
-          </p>
-
-          <HelloWorld />
-        </header>
         <body>
           <CountBoard />
         </body>
