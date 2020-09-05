c76b8728562fd08ead7a973422fb0e2356a683ec
Hsakihara <sakiharu31415@yahoo.co.jp>
2020-09-05 16:31:33 +0900

    material-ui-dropzoneの導入

--- frontend/src/components/DropBox.js
+++ frontend/src/components/DropBox.js
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react';
-import {useDropzone} from 'react-dropzone';
 import { AppContext } from '../contexts/appContexts';
 import { multiOnButton } from '../actions/actionCreaters';
+import { DropzoneAreaBase } from 'material-ui-dropzone';
 
 
 function DropBox () {
@@ -19,27 +19,14 @@ function DropBox () {
         });
     }
 
-    const {acceptedFiles, getRootProps, getInputProps} = useDropzone({
-        accept: "image/jpeg,image/png,image/jpg",
-        onDrop: handleOnDrop
-    });
-  
-    const files = acceptedFiles.map(file => (
-        <li key={file.path}>
-        {file.path} - {file.size} bytes
-        </li>
-    ));
-
     return (
         <section className="container">
-            <div {...getRootProps({className: 'dropzone'})}>
-                <input {...getInputProps()} />
-                <p>Drag 'n' drop some files here, or click to select files</p>
-            </div>
-            <aside>
-                <h4>Files</h4>
-                <ul>{files}</ul>
-            </aside>
+            <DropzoneAreaBase
+                acceptedFiles={['image/png']}
+                dropzoneText={"Drag and drop an image here or click"}
+                onDrop={(files) => handleOnDrop(files)}
+                onAlert={(message, variant) => console.log(`${variant}: ${message}`)}
+            />
         </section>
     );
 }
